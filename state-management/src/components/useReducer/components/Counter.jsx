caeb18735a12bb00d94d35c0392ad451bb2d1fad
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-09-22 12:41:28 +0600

    m

--- state-management/src/components/useReducer/components/Counter.jsx
+++ state-management/src/components/useReducer/components/Counter.jsx
@@ -17,12 +17,18 @@ const Counter = () => {
     dispatch({ type: ACTIONS.INCREASEBY5, payload: { number: 5 } });
   };
 
+  const toogleName = ()=> {
+    dispatch({type: ACTIONS.TOGLENAME})
+  }
+
   return (
     <div>
       <h2>Count: {state.count}</h2>
+      <h2>Name: {state.name}</h2>
       <button onClick={increaseCounter}>+</button>
       <button onClick={decreaseCounter}>-</button>
       <button onClick={increaseBy5}>Increase 5 </button>
+      <button onClick={toogleName}>change The Name</button>
     </div>
   );
 };
--- state-management/src/components/useReducer/reducers/counterReducer.js
+++ state-management/src/components/useReducer/reducers/counterReducer.js
@@ -1,17 +1,20 @@
 import { ACTIONS } from "../constants/actions";
 
-export const initialState = { count: 0 };
+export const initialState = { count: 0, name: "kajol" };
 
 export const counterReducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.INCREMENT:
-      return { count: state.count + 1 };
+      return {...state, count: state.count + 1 };
 
     case ACTIONS.DECREMENT:
-      return { count: state.count - 1 };
+      return {...state, count: state.count - 1 };
 
     case ACTIONS.INCREASEBY5:
-      return { count: state.count + action.payload.number };
+      return {...state, count: state.count + action.payload.number };
+
+    case ACTIONS.TOGLENAME: 
+      return {...state, name: state.name =="Ali" ? "kajol" : "Ali"}    
 
     default:
       return state;
